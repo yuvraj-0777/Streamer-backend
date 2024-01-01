3732d0f432f620f7229c9eaa6e0a7458904524eb
yuvraj-0777 <yuvsinh0777@gmail.com>
2024-01-02 01:13:00 +0530

    Added Models

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudnary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, //cloudnary url
+            required: true,
+        },
+        owner: {
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, //using cloudnary
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        }
+    }, {timestamps: true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
